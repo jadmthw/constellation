ceaf1e236603d36508ebdbb07f114dc21b9d5b42
Jaden Mathew <jadenmath@gmail.com>
2026-08-24 05:22:57 +0000

    feat: add Stack
    Co-authored-by: rstevebean <155026264+rstevebean@users.noreply.github.com>

---
+++ src/stack.ts
@@ -0,0 +1,23 @@
+export class Stack<T> {
+  private items: T[] = [];
+
+  push(item: T): void {
+    this.items.push(item);
+  }
+
+  pop(): T | undefined {
+    return this.items.pop();
+  }
+
+  peek(): T | undefined {
+    return this.items[this.items.length - 1];
+  }
+
+  get size(): number {
+    return this.items.length;
+  }
+
+  get empty(): boolean {
+    return this.items.length === 0;
+  }
+}
